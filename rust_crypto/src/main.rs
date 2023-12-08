ba89a59385e21b56711035a763a731d27705093b
Niels <nielsmpro@gmail.com>
2023-12-08 14:28:28 +0100

    def of function and variable for square and multiply

--- rust_crypto/src/main.rs
+++ rust_crypto/src/main.rs
@@ -1,3 +1,8 @@
+mod square_and_multiply;
+use square_and_multiply::sam;
+
 fn main() {
-    println!("Hello, world!");
+    let tmp : u64 = sam(10,20,30);
+
+    println!("retour {tmp}");
 }
---
+++ rust_crypto/src/square_and_multiply.rs
@@ -0,0 +1,17 @@
+//use std::io;
+
+//change a signed integer into a binary array
+fn integer_64_to_bit(y: u64) -> [i8; 64]{
+
+    let tab : [i8; 64] = [0; 64];
+    
+    return tab;
+}
+
+// expo modu (x puiss y) mod n
+pub fn sam(x: u64, y: u64 , n: u64) -> u64{
+   
+
+
+    return 10;
+}
