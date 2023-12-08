14f6b5b5d4b730c09cbde20369ef69acc6a96b4e
Niels <nielsmpro@gmail.com>
2023-12-08 15:31:08 +0100

    function integer_64_to_bit done , need to add documentation for this function

--- rust_crypto/src/main.rs
+++ rust_crypto/src/main.rs
@@ -2,7 +2,7 @@ mod square_and_multiply;
 use square_and_multiply::sam;
 
 fn main() {
-    let tmp : u64 = sam(10,20,30);
+    let tmp : u64 = sam(10,605,30);
 
     println!("retour {tmp}");
 }
--- rust_crypto/src/square_and_multiply.rs
+++ rust_crypto/src/square_and_multiply.rs
@@ -1,17 +1,35 @@
-//use std::io;
+use std::vec::Vec;
 
 //change a signed integer into a binary array
-fn integer_64_to_bit(y: u64) -> [i8; 64]{
+fn integer_64_to_bit(y: u64) -> Vec<i8>{
 
-    let tab : [i8; 64] = [0; 64];
     
-    return tab;
+    let mut tmp_y : u64 = y;
+    let mut binar: Vec<i8> = Vec::new();  
+  
+
+    while tmp_y!=0 {
+        if (tmp_y%2)!=0{
+            tmp_y=tmp_y-1;
+            tmp_y=tmp_y/2;
+            binar.push(1);
+        }
+        else{
+           
+            tmp_y=tmp_y/2;
+            binar.push(0);
+        }
+        
+    }
+    return binar;
 }
 
 // expo modu (x puiss y) mod n
 pub fn sam(x: u64, y: u64 , n: u64) -> u64{
    
-
-
+    let mut binar: Vec<i8> = Vec::new();
+    binar= integer_64_to_bit(y);
+    println!("binaire de la puissance:{:?}",binar);
+    
     return 10;
 }
