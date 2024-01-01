03968af3668f6bdb2752c3243d8815533cb25058
niels <nielsmpro@gmail.com>
2024-01-01 18:04:52 +0100

    gestion d'erreur sur l'input

--- rust_crypto/src/main.rs
+++ rust_crypto/src/main.rs
@@ -5,6 +5,8 @@ use square_and_multiply_unlimited::square_and_multiply_unlimited::samu;
 
 fn main() {
     let tmp : u64 = sam_u64(5,1000,44);
+
+    samu("1235","AAAZEZRS","azzarsd")
     
 
     //println!("résultat expo mod:{tmp}");
--- rust_crypto/src/square_and_multiply_unlimited.rs
+++ rust_crypto/src/square_and_multiply_unlimited.rs
@@ -1,11 +1,23 @@
 
-
 //The purpose of this code is to perform a square and multiply on integers larger than u64.
 pub mod square_and_multiply_unlimited {
 
+    fn acceptchar(input: &str) -> bool{
+        for i in input.chars() {
+            if(i>='0' && i<='9') || (i>='A' && i<='Z') || (i>='a' && i<='z'){}
+            else {return false}
+        }
+        return true
+    }
     // expo modu (x puiss y) mod n
-    pub fn samu(){
+    pub fn samu(inputa: &str, inputn: &str, inputpuiss: &str) {
         
+        if( acceptchar(inputa) && acceptchar(inputn) && acceptchar(inputpuiss)){
+            println!("input correct")
+        }
+        else{
+            println!("input incorrect, use [0;9] U [a;z] U [A;Z]")
+        }
     
         
     }
